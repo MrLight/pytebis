0903fe465f019f2a0408d71d61b87506a20f8d52
MrLight <sebastian.balz@gmail.com>
2023-06-14 10:28:13 +0200

    add raw connection, add microsec timestamps

--- pytebis/tebis.py
+++ pytebis/tebis.py
@@ -78,18 +78,26 @@ class Tebis():
                 ids.append(id)
         if isinstance(start, datetime.datetime):
             start = start.timestamp()
+        elif isinstance(start, float):
+            start = start*1000.0
+        elif isinstance(start, int) and start < 100000000000: #and start > 100000000000 and start < 100000000000000:
+            start = start*1000.0
         elif isinstance(start, str):
             start = datetime.datetime.strptime(start, '%Y-%m-%d %H:%M:%S.%f').timestamp()
         if isinstance(end, datetime.datetime):
             end = end.timestamp()
+        elif isinstance(end, float):
+            end = end*1000.0
+        elif isinstance(end, int) and end < 100000000000: #and start > 100000000000 and start < 100000000000000:
+            end = end*1000.0
         elif isinstance(end, str):
             end = datetime.datetime.strptime(end, '%Y-%m-%d %H:%M:%S.%f').timestamp()
 
         nCT = rate*1000.0
-        nTimeR = end*1000.0
+        nTimeR = end
         nTimeR = (int(float(nTimeR)) / int(nCT)) * int(nCT)
 
-        nTimeL = start*1000.0
+        nTimeL = start
         nTimeL = (int(float(nTimeL)) / int(nCT)) * int(nCT)
         nNmbX = int(nTimeR - nTimeL) / int(nCT)
         if nNmbX <= 0:
@@ -97,17 +105,60 @@ class Tebis():
         
         return self.__getBinData(ids=ids, nNmbX=nNmbX, TimeR=nTimeR, nCT=nCT/1000.0)
 
-    # TODO: implement return RawData for Client based Converters like Javascript
-    def getRawData(self, names, start, end, rate=1):
-        return None
 
     def getDataAsJson(self, names, start, end, rate=1):
         return getDataSeries_as_Json(self.getDataAsNP(names, start, end, rate))
 
+    # returns RawData for Client based Converters like Javascript
+    def getDataRAW(self,filepath, names, start, end, rate=1):
+        ids = []
+        # find Mst with id as a number, id as MST name a str, id
+        for name in names:
+            id = None
+            if isinstance(name, numbers.Number):
+                id = self.getMst(id=name).id
+            elif isinstance(name, str):
+                id = self.getMst(name=name).id
+            elif isinstance(name, TebisMST):
+                id = name.id
+            elif isinstance(name, TebisGroupElement):
+                for member in name.members:
+                    ids.append(member.mst.id)
+            elif isinstance(name, TebisGroupMember):
+                id = name.mst.id
+            if id is not None:
+                ids.append(id)
+        if isinstance(start, datetime.datetime):
+            start = start.timestamp()
+        elif isinstance(start, float):
+            start = start*1000.0
+        elif isinstance(start, int) and start < 100000000000: #and start > 100000000000 and start < 100000000000000:
+            start = start*1000.0
+        elif isinstance(start, str):
+            start = datetime.datetime.strptime(start, '%Y-%m-%d %H:%M:%S.%f').timestamp()
+        if isinstance(end, datetime.datetime):
+            end = end.timestamp()
+        elif isinstance(end, float):
+            end = end*1000.0
+        elif isinstance(end, int) and end < 100000000000: #and start > 100000000000 and start < 100000000000000:
+            end = end*1000.0
+        elif isinstance(end, str):
+            end = datetime.datetime.strptime(end, '%Y-%m-%d %H:%M:%S.%f').timestamp()
 
+        nCT = rate*1000.0
+        nTimeR = end
+        nTimeR = (int(float(nTimeR)) / int(nCT)) * int(nCT)
+
+        nTimeL = start
+        nTimeL = (int(float(nTimeL)) / int(nCT)) * int(nCT)
+        nNmbX = int(nTimeR - nTimeL) / int(nCT)
+        if nNmbX <= 0:
+            nNmbX = 1
+        
+        return self.getBinDataRAW(filepath,ids=ids, nNmbX=nNmbX, TimeR=nTimeR, nCT=nCT/1000.0)
 
     def getDataAsPD(self, names, start, end = None, rate=1):
-        if all(isinstance(elem, list) for elem in start):
+        if isinstance(start, list) and all(isinstance(elem, list) for elem in start):
             df = None
             for tuple in start:
                 if df is None:
@@ -752,7 +803,73 @@ class Tebis():
         return b''.join(chunks)
 
 # endregion
-
+    def getBinDataRAW(self, filepath, ids=None, nCT=1, nNmbX=1, TimeR=time.time()):
+        
+        start = round(time.time() * 1000)
+        #TimeR = TimeR * 1000.0
+        nCT = int(nCT*1000.0)
+        nCT = self.checkIfReductionAvailable(nCT)
+        if TimeR > start:
+            dif = int(TimeR - start) / int(nCT)
+            TimeR = int(start)
+            nNmbX = int(nNmbX - dif)
+        timeR_new = int(int(int(int(TimeR) / int(nCT)) * int(nCT)))
+        dif = int(int(int(TimeR) - timeR_new) / int(nCT))
+        nNmbX = int(nNmbX - dif)
+        n = 50
+        data = None
+        types = [('timestamp', (np.int64))]
+        for id in ids:
+            mst = self.getMst(id=id)
+            types.append((str(mst.name), (np.float32)))
+        x = [ids[i:i + n] for i in range(0, len(ids), n)]
+        offset = 0
+        
+        for ids in x:
+            rawdata = bytearray()
+            arrMsts = ""
+            rawdata.extend(len(ids).to_bytes(8,'big'))
+            for id in ids:
+                arrMsts += str(id) + ', '
+                rawdata.extend(id.item().to_bytes(8,'big'))
+            arrMsts = arrMsts[:-2]
+            #print(arrMsts)
+            if nNmbX > 0:
+                strRequest = "<tebis>\n"
+                strRequest += "<szConfigFile>" + \
+                    self.config['configfile'] + "</szConfigFile>\n"
+                strRequest += "<szProcedure>LoadData</szProcedure>\n"
+                strRequest += "<arrMsts>" + arrMsts + "</arrMsts>\n"
+                strRequest += "<nNmbX>" + str(nNmbX) + "</nNmbX>\n"
+                strRequest += "<nCT>" + str(int(nCT)) + "</nCT>\n"
+                strRequest += "<nTimeR>" + \
+                    str(timeR_new) + "</nTimeR>\n"
+                strRequest += "<tebis>"
+                try:
+                    self.socketConnect()
+                    # Send Request
+                    self.sendOnSocket(strRequest)
+                    # Recieve MSTS Packet
+                    MSTSRaw = self.receiveOnSocket()
+                except TebisException:
+                    self.socketConnect()
+                    # Send Request
+                    self.sendOnSocket(strRequest)
+                    # Recieve MSTS Packet
+                    MSTSRaw = self.receiveOnSocket()  
+                rawdata.extend(len(MSTSRaw).to_bytes(8,'big'))
+                rawdata.extend(MSTSRaw)
+                #print(len(ids)) 
+                #print(MSTSRaw)
+                #data = self.__checkBinaryResultHeader(
+                #    MSTSRaw, types, data, offset)
+                #offset += len(ids)
+            #data['timestamp'] = data['timestamp'] / 1000.0
+            with open(filepath, 'ab') as fpout:
+                fpout.write(rawdata)
+            #print(os.path.getsize(filepath))
+        return rawdata
+    
     """
     schnelles Lesen von Messreihen
     ids= Array mit den Messtellen-Namen
